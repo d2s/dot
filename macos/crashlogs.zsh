f8e83ba88f98fc52c1f25a11db45d67f00aa2a96
Daniel Schildt <daniel.schildt@gmail.com>
2016-11-03 01:54:33 +0200

    feat(crashlogs) add zsh aliases for crashlogs
    - Add zsh aliases for handling macOS crashlogs
    
    Based on old configs:
    https://github.com/d2s/dotfiles/blob/master/etc/bashrc#L369

---
+++ macos/crashlogs.zsh
@@ -0,0 +1,33 @@
+# -----------------------------------------------------------
+# CRASHLOGS
+# macOS Crash logs / Diagnostic Reports
+
+# macOS Diagnostic Reports
+if [ -d /Library/Logs/DiagnosticReports ]; then
+  alias maccrashlogs='cd /Library/Logs/DiagnosticReports'
+fi
+
+# macOS user Diagnostic Reports
+if [ -d ~/Library/Logs/DiagnosticReports ]; then
+  alias macusercrashlogs='cd ~/Library/Logs/DiagnosticReports'
+fi
+
+# macOS user logs
+if [ -d ~/Library/Logs ]; then
+  alias macuserlogs="cd ~/Library/Logs"
+fi
+
+
+# Shortcut to crashlogs_backup directory if there is such
+if [ -d ~/Documents/BACKUPS/crashlogs_backup ]; then
+  alias backups-crashlogs="cd ~/Documents/BACKUPS/crashlogs_backup"
+fi
+
+
+# Make backup of crashlogs to ~/Documents/BACKUPS/crashlogs_backup directory
+# Checks first if there are both original source and end-location for files.
+if [ -d ~/Library/Logs/DiagnosticReports -a -d ~/Documents/BACKUPS/crashlogs_backup ]; then
+  crashlogs-backup() { cp -anv ~/Library/Logs/DiagnosticReports/* ~/Documents/BACKUPS/crashlogs_backup;
+                       cp -anv /Library/Logs/DiagnosticReports/* ~/Documents/BACKUPS/crashlogs_backup;
+                     }
+                   fi
