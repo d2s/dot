50eb04bb6109c650a7947a6b9946bf6a71b41fa7
Daniel Schildt <daniel.schildt@gmail.com>
2016-11-03 00:24:30 +0200

    fix(zsh) improve aliases
    - Modify zsh aliases for listing directory contents.
    
    New changes are based on old configs from:
    - https://github.com/d2s/dotfiles/blob/master/etc/bashrc#L203

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,11 @@
 # -----------------------------------------------------------
 # List directory contents faster
+# (for extra details, look at `man ls` documentation)
 alias l="ls"
-alias ll="ls -la"
-alias la="ls -a"
+alias ll="ls -hl"
+alias la='ls -A'
+alias lla='ls -lah'
+alias l='ls -CF'
 
 
 # -----------------------------------------------------------
