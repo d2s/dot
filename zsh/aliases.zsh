382146db4415271ff4a41dbdecf78c310f659888
Daniel Schildt <daniel.schildt@gmail.com>
2016-11-02 23:53:33 +0200

    feat(zsh) add new aliases
    - Add new zsh aliases for listing directory contents.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,11 @@
+# -----------------------------------------------------------
+# List directory contents faster
+alias l="ls"
+alias ll="ls -la"
+alias la="ls -a"
+
+
+# -----------------------------------------------------------
+# Jump to favorite locations
+alias dotfiles="cd $ZSH"
+alias projects="cd $PROJECTS"
