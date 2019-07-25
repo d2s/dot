7e1ab410d271009872d97005f16acbc7545b59c8
Daniel Schildt <daniel.schildt@gmail.com>
2019-07-25 16:37:31 +0300

    feat(macos): add more tools from Homebrew
    - Add more tools from Homebrew to the installer script

--- macos/brew-install.sh
+++ macos/brew-install.sh
@@ -7,7 +7,7 @@ if [ -x "$(command -v brew)" ] ; then
   echo ""
   echo "Installing tools with Homebrew:"
   echo ""
-  brew install youtube-dl yarn wget hugo
+  brew install zsh git youtube-dl yarn wget hugo hub tree
 else
   echo ""
   echo "You need to have Homebrew installed (before installing other tools with it)."
