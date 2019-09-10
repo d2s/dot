1bd101a98bf85dd0bfdf9d57f2aef42c5bf31be8
Daniel Schildt <daniel@autiomaa.org>
2019-09-10 23:09:24 +0300

    feat(scripts): add installer for apt packages
    - Add installer for apt packages
      - Check whether command already exists
      - If command is not existing, install package
      - If command exists, print note about it to the console

---
+++ scripts/install-apt-packages.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -
+###############################################################################
+# install-apt-packages.sh
+# This script installs tools from apt packages if possible
+###############################################################################
+
+# -----------------------------------------------------------
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\t\n' # Stricter IFS settings
+ORIGINAL_IFS=$IFS
+
+# -----------------------------------------------------------
+# Go to user account's home directory
+cd $HOME
+
+# -----------------------------------------------------------
+# Commonly used text strings for the script
+NOTE_APT_INSTALL="apt install:"
+NOTE_COMMAND_EXISTS_ALREADY="NOTE: already installed:"
+
+# -----------------------------------------------------------
+# TODO: Make a wrapper function that improves the installation process consistency
+# APT_COMMAND_FOR_INSTALLING="$(sudo apt install)"
+
+
+
+# -----------------------------------------------------------
+# If `apt` command is available
+if [ -x "$(command -v apt)" ]; then
+
+  # -----------------------------------------------------------
+  # `whois` records search tool
+
+  # What is the apt package name for the application?
+  APT_PACKAGES_WHOIS="whois"
+
+  # If CLI command is not already existing
+  if [ ! -x "$(command -v $APT_PACKAGES_WHOIS)" ]; then
+    # Install related npm packages to get the application
+    echo "$NOTE_APT_INSTALL $APT_PACKAGES_WHOIS"
+    sudo apt install $APT_PACKAGES_WHOIS
+  else
+    echo "$NOTE_COMMAND_EXISTS_ALREADY $APT_PACKAGES_WHOIS"
+  fi
+
+  # -----------------------------------------------------------
+  # ...
+
+else
+  echo "NOTE: apt is not available! Nothing happened."
+fi
