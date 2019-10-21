c1ab5c641bba76f9c2f45546df4230f1e4496ba3
Daniel Schildt <daniel@autiomaa.org>
2019-10-21 18:32:12 +0300

    feat(scripts): add Yarn package: `@storybook/cli`
    - Add Yarn package: `@storybook/cli`
      - Storybook CLI
    - Create simple installer script for global Yarn packages
    
    More details:
    - https://www.npmjs.com/package/@storybook/cli
    - https://storybook.js.org/docs/guides/quick-start-guide/
    - https://storybook.js.org/docs/guides/guide-react/

---
+++ scripts/install-yarn-packages.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -
+###############################################################################
+# install-npm-packages.sh
+# This script installs npm packages if `npm` command is available
+###############################################################################
+
+# -----------------------------------------------------------
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\t\n' # Stricter IFS settings
+ORIGINAL_IFS=$IFS
+
+# -----------------------------------------------------------
+# Go to user account's home directory
+cd "$HOME"
+
+# -----------------------------------------------------------
+# Commonly used text strings for the script
+NOTE_YARN_INSTALL="yarn install:"
+NOTE_COMMAND_EXISTS_ALREADY="NOTE: already installed:"
+
+# -----------------------------------------------------------
+# TODO: Make a wrapper function that improves the installation process consistency
+# NPM_COMMAND_FOR_INSTALLING="$(npm install -g)"
+
+
+
+# -----------------------------------------------------------
+# If npm command is available
+if [ -x "$(command -v yarn)" ]; then
+
+  # -----------------------------------------------------------
+  # Storybook
+  # - https://storybook.js.org/docs/guides/quick-start-guide/
+  # - https://storybook.js.org/docs/guides/guide-react/
+  # - https://www.npmjs.com/package/@storybook/cli
+  #
+  # What is the CLI command for the application?
+  YARN_PACKAGES_STORYBOOK_COMMAND="sb"
+
+  # What is the npm package for the application?
+  YARN_PACKAGES_STORYBOOK="@storybook/cli"
+
+  # If CLI command is not already existing
+  if [ ! -x "$(command -v $YARN_PACKAGES_STORYBOOK_COMMAND)" ]; then
+    # Install related npm packages to get the application
+    echo "$NOTE_YARN_INSTALL $YARN_PACKAGES_STORYBOOK"
+    yarn global add $YARN_PACKAGES_STORYBOOK
+  else
+    echo "$NOTE_COMMAND_EXISTS_ALREADY $YARN_PACKAGES_STORYBOOK_COMMAND"
+  fi
+
+  # -----------------------------------------------------------
+  # ...
+
+else
+  echo "NOTE: Yarn is not installed! Nothing happened."
+fi
