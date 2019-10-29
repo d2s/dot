6266320dc60a8b93f4231860a342531063f95d8d
Daniel Schildt <daniel@autiomaa.org>
2019-10-29 18:46:36 +0200

    feat(scripts): rewrite apt package installer script
    - Rewrite apt package installer script
      - Use `dpkg` for checking if package is already installed
      - Compare `dpkg` output to a string
      - Add empty lines between terminal outputs for improved readability
    - Check the script structure with
    - Ignore `shellcheck` SC2034 warning on line 13
      because unused variable is needed for the Bash strict mode
      - Only one line with `ORIGINAL_IFS` is ignored,
        rest of the file is still checked
    
    More details:
    - https://linuxhint.com/compare_strings_bash/
    - https://stackoverflow.com/questions/2237080/how-to-compare-strings-in-bash
    - https://github.com/koalaman/shellcheck
    - https://github.com/koalaman/shellcheck/wiki/SC2034
    - https://github.com/koalaman/shellcheck/wiki/Ignore

--- scripts/install-apt-packages.sh
+++ scripts/install-apt-packages.sh
@@ -9,6 +9,7 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\t\n' # Stricter IFS settings
+# shellcheck disable=SC2034  # Unused variable needed for the Bash strict mode
 ORIGINAL_IFS=$IFS
 
 # -----------------------------------------------------------
@@ -17,16 +18,20 @@ cd "$HOME"
 
 # -----------------------------------------------------------
 # Commonly used text strings for the script
-NOTE_APT_INSTALL="apt install:"
-NOTE_COMMAND_EXISTS_ALREADY="NOTE: already installed:"
+NOTE_APT_INSTALL="sudo apt install"
+NOTE_PACKAGE_EXISTS_ALREADY="NOTE: package already installed:"
+NOTE_LINESPACE=""
+
+# String to compare to check if package already installed
+PACKAGE_STATUS_INSTALL_OK="Status: install ok installed"
 
 # -----------------------------------------------------------
 # TODO: Make a wrapper function that improves the installation process consistency
-# APT_COMMAND_FOR_INSTALLING="$(sudo apt install)"
 
 # -----------------------------------------------------------
 # If `apt` command is available
 if [ -x "$(command -v apt)" ]; then
+  echo "$NOTE_LINESPACE"
 
   # -----------------------------------------------------------
   # `whois` records search tool
@@ -34,13 +39,16 @@ if [ -x "$(command -v apt)" ]; then
   # What is the apt package name for the application?
   APT_PACKAGES_WHOIS="whois"
 
-  # If CLI command is not already existing
-  if [ ! -x "$(command -v $APT_PACKAGES_WHOIS)" ]; then
+  # If the package is not already installed
+  if [[ ! "$(dpkg -s $APT_PACKAGES_WHOIS | grep Status)" == "$PACKAGE_STATUS_INSTALL_OK" ]]; then
+    echo "$NOTE_LINESPACE"
     # Install related apt packages to get the application
     echo "$NOTE_APT_INSTALL $APT_PACKAGES_WHOIS"
-    sudo apt install $APT_PACKAGES_WHOIS
+    sudo apt install "$APT_PACKAGES_WHOIS"
+    echo "$NOTE_LINESPACE"
   else
-    echo "$NOTE_COMMAND_EXISTS_ALREADY $APT_PACKAGES_WHOIS"
+    echo "$NOTE_PACKAGE_EXISTS_ALREADY $APT_PACKAGES_WHOIS"
+    echo "$NOTE_LINESPACE"
   fi
 
   # -----------------------------------------------------------
@@ -50,13 +58,16 @@ if [ -x "$(command -v apt)" ]; then
   # What is the apt package name for the application?
   APT_PACKAGES_SHELLCHECK="shellcheck"
 
-  # If CLI command is not already existing
-  if [ ! -x "$(command -v $APT_PACKAGES_SHELLCHECK)" ]; then
+  # If the package is not already installed
+  if [[ ! "$(dpkg -s $APT_PACKAGES_SHELLCHECK | grep Status)" == "$PACKAGE_STATUS_INSTALL_OK" ]]; then
+    echo "$NOTE_LINESPACE"
     # Install related apt packages to get the application
     echo "$NOTE_APT_INSTALL $APT_PACKAGES_SHELLCHECK"
-    sudo apt install $APT_PACKAGES_SHELLCHECK
+    sudo apt install "$APT_PACKAGES_SHELLCHECK"
+    echo "$NOTE_LINESPACE"
   else
-    echo "$NOTE_COMMAND_EXISTS_ALREADY $APT_PACKAGES_SHELLCHECK"
+    echo "$NOTE_PACKAGE_EXISTS_ALREADY $APT_PACKAGES_SHELLCHECK"
+    echo "$NOTE_LINESPACE"
   fi
 
   # -----------------------------------------------------------
@@ -65,32 +76,36 @@ if [ -x "$(command -v apt)" ]; then
   # What is the apt package name for the application?
   APT_PACKAGES_PCREGREP="pcregrep"
 
-  # If CLI command is not already existing
-  if [ ! -x "$(command -v $APT_PACKAGES_PCREGREP)" ]; then
+  # If the package is not already installed
+  if [[ ! "$(dpkg -s $APT_PACKAGES_PCREGREP | grep Status)" == "$PACKAGE_STATUS_INSTALL_OK" ]]; then
+    echo "$NOTE_LINESPACE"
     # Install related apt packages to get the application
     echo "$NOTE_APT_INSTALL $APT_PACKAGES_PCREGREP"
-    sudo apt install $APT_PACKAGES_PCREGREP
+    sudo apt install "$APT_PACKAGES_PCREGREP"
+    echo "$NOTE_LINESPACE"
   else
-    echo "$NOTE_COMMAND_EXISTS_ALREADY $APT_PACKAGES_PCREGREP"
+    echo "$NOTE_PACKAGE_EXISTS_ALREADY $APT_PACKAGES_PCREGREP"
+    echo "$NOTE_LINESPACE"
   fi
 
   # -----------------------------------------------------------
   # DNS query tools (including `dig`)
 
-  # What is the command for starting the application?
-  APT_PACKAGES_DNS_COMMAND="dig"
-
   # What is the apt package name for the application?
   APT_PACKAGES_DNS="dnsutils"
 
-  # If CLI command is not already existing
-  if [ ! -x "$(command -v $APT_PACKAGES_DNS_COMMAND)" ]; then
+  # If the package is not already installed
+  if [[ ! "$(dpkg -s $APT_PACKAGES_DNS | grep Status)" == "$PACKAGE_STATUS_INSTALL_OK" ]]; then
+    echo "$NOTE_LINESPACE"
     # Install related apt packages to get the application
     echo "$NOTE_APT_INSTALL $APT_PACKAGES_DNS"
-    sudo apt install $APT_PACKAGES_DNS
+    sudo apt install "$APT_PACKAGES_DNS"
+    echo "$NOTE_LINESPACE"
   else
-    echo "$NOTE_COMMAND_EXISTS_ALREADY $APT_PACKAGES_DNS_COMMAND"
+    echo "$NOTE_PACKAGE_EXISTS_ALREADY $APT_PACKAGES_DNS"
+    echo "$NOTE_LINESPACE"
   fi
+
 
   # -----------------------------------------------------------
   # Chromium (web browser)
@@ -101,13 +116,16 @@ if [ -x "$(command -v apt)" ]; then
   # What is the apt package name(s) for the application?
   APT_PACKAGES_CHROMIUM="chromium chromium-sandbox"
 
-  # If CLI command is not already existing
-  if [ ! -x "$(command -v $APT_PACKAGES_CHROMIUM_COMMAND)" ]; then
+  # If the package is not already installed
+  if [[ ! "$(dpkg -s $APT_PACKAGES_CHROMIUM_COMMAND | grep Status)" == "$PACKAGE_STATUS_INSTALL_OK" ]]; then
+    echo "$NOTE_LINESPACE"
     # Install related apt packages to get the application
     echo "$NOTE_APT_INSTALL $APT_PACKAGES_CHROMIUM"
-    sudo apt install $APT_PACKAGES_CHROMIUM
+    sudo apt install "$APT_PACKAGES_CHROMIUM"
+    echo "$NOTE_LINESPACE"
   else
-    echo "$NOTE_COMMAND_EXISTS_ALREADY $APT_PACKAGES_CHROMIUM_COMMAND"
+    echo "$NOTE_PACKAGE_EXISTS_ALREADY $APT_PACKAGES_CHROMIUM_COMMAND"
+    echo "$NOTE_LINESPACE"
   fi
 
   # -----------------------------------------------------------
