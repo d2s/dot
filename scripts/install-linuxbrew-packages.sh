88824adfcf999edc4aaa634be8d1442ae5b6ffc2
Daniel Schildt <daniel@autiomaa.org>
2020-01-23 06:28:43 +0200

    feat(scripts): rewrite Linuxbrew package installer
    - Rewrite Linuxbrew package installer script
      - Use Bash functions to reduce amount of duplication
      - Reduce amount of lines
      - Improve readability
    - Add new packages:
      - Add package: `hub`
    - Fix `shellcheck` warning

--- scripts/install-linuxbrew-packages.sh
+++ scripts/install-linuxbrew-packages.sh
@@ -9,6 +9,7 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\t\n' # Stricter IFS settings
+# shellcheck disable=SC2034  # Unused variable needed for the Bash strict mode
 ORIGINAL_IFS=$IFS
 
 # -----------------------------------------------------------
@@ -16,34 +17,56 @@ ORIGINAL_IFS=$IFS
 cd "$HOME"
 
 # -----------------------------------------------------------
-# Commonly used text strings for the script
-NOTE_LINUXBREW_INSTALL="brew install:"
-NOTE_COMMAND_EXISTS_ALREADY="NOTE: already installed:"
+# Update apt package lists
+# Usage:
+#   brew_update
+brew_update() {
+  printf "\\n"
+  printf "Updating Homebrew package lists:"
+  printf "\\n"
+  brew update
+  printf "\\n"
+}
 
 # -----------------------------------------------------------
-# TODO: Make a wrapper function that improves the installation process consistency
-# LINUXBREW_COMMAND_FOR_INSTALLING="$(brew install)"
+# Install package defined in a string variable
+# Usage:
+#   install_brew_package "package"
+install_brew_package() {
+  printf "\\n"
+  printf "brew install %s\\n" "$1"
+  brew install "$1"
+  printf "\\n\\n"
+}
+
+# -----------------------------------------------------------
+# Tell user that package has already been installed
+# Usage:
+#   brew_package_already_installed "package"
+brew_package_already_installed() {
+  printf "NOTE: package already installed: %s\\n" "$1"
+  printf "\\n"
+}
+
+# -----------------------------------------------------------
+# If package is not already installed, install it
+# Usage:
+#   if_not_already_installed "package"
+if_not_already_installed() {
+  if [[ ! "$(brew list |grep "$1")" == "$1" ]]; then
+    install_brew_package "$1"
+  else
+    brew_package_already_installed "$1"
+  fi
+}
 
 # -----------------------------------------------------------
 # If `brew` command is available
 if [ -x "$(command -v brew)" ]; then
+  brew_update
 
-  # -----------------------------------------------------------
-  # `youtube-dl`
-  # What is the package name for the application?
-  LINUXBREW_PACKAGES_YT="youtube-dl"
-
-  # If CLI command is not already existing
-  if [ ! -x "$(command -v $LINUXBREW_PACKAGES_YT)" ]; then
-    # Install related npm packages to get the application
-    echo "$NOTE_LINUXBREW_INSTALL $LINUXBREW_PACKAGES_YT"
-    brew install $LINUXBREW_PACKAGES_YT
-  else
-    echo "$NOTE_COMMAND_EXISTS_ALREADY $LINUXBREW_PACKAGES_YT"
-  fi
-
-  # -----------------------------------------------------------
-  # ...
+  if_not_already_installed "hub"
+  if_not_already_installed "youtube-dl"
 
 else
   echo ""
