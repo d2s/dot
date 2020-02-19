95fc2c4922839754d1747b937dfcb6a70123ae64
Daniel Schildt <daniel@autiomaa.org>
2020-02-19 23:38:33 +0200

    feat(scripts): add Snapcraft package installer
    - Add Snapcraft package installer
    
    More details:
    - https://snapcraft.io/

---
+++ scripts/install-snap-packages.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+#############################################################
+# install-snap-packages.sh
+# Script installs npm packages with `snap` if it is available
+#############################################################
+
+# -----------------------------------------------------------
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# -----------------------------------------------------------
+set -euo pipefail
+IFS=$'\t\n' # Stricter IFS settings
+# shellcheck disable=SC2034  # Unused variable needed for the Bash strict mode
+ORIGINAL_IFS=$IFS
+
+# -----------------------------------------------------------
+# Go to user account's home directory
+# -----------------------------------------------------------
+cd "$HOME"
+
+# -----------------------------------------------------------
+# Install package defined in a string variable
+# Usage:
+#   install_snap_package "package"
+# -----------------------------------------------------------
+install_snap_package() {
+  printf "\\n"
+  printf "sudo snap install %s\\n" "$1"
+  sudo snap install "$1"
+  printf "\\n\\n"
+}
+
+# -----------------------------------------------------------
+# Tell user that package has already been installed
+# Usage:
+#   snap_package_already_installed "package"
+# -----------------------------------------------------------
+snap_package_already_installed() {
+  # printf "NOTE: package already installed: %s\\n" "$1"
+  printf "NOTE: command already exists: %s\\n" "$1"
+  printf "\\n"
+}
+
+# -----------------------------------------------------------
+# If package is not already installed, install it
+# Usage:
+#   if_not_already_installed "package"
+# -----------------------------------------------------------
+if_not_already_installed() {
+  if [ ! -x "$(command -v "$1")" ]; then
+    install_snap_package "$1"
+  else
+    snap_package_already_installed "$1"
+  fi
+}
+
+# -----------------------------------------------------------
+# If CLI command is not available, install npm package
+# Usage:
+#   if_cli_command_does_not_exist "command_name" "package_name" "extra_note"
+# -----------------------------------------------------------
+if_cli_command_does_not_exist() {
+  if [ ! -x "$(command -v "$1")" ]; then
+    if_not_already_installed "$2"
+
+    if [ "$3" ]; then
+      printf "NOTE: %s\\n" "$3"
+      printf "\\n"
+    fi
+  else
+    snap_package_already_installed "$1"
+  fi
+}
+
+# -----------------------------------------------------------
+# If snap command is available
+if [ -x "$(command -v snap)" ]; then
+  # -----------------------------------------------------------
+  # "Streaming music player that finds music sources automatically."
+  # - https://github.com/nukeop/nuclear
+  # - https://snapcraft.io/nuclear
+  # -----------------------------------------------------------
+  if_not_already_installed "nuclear"
+
+  # -----------------------------------------------------------
+  # Storybook
+  # -----------------------------------------------------------
+  # if_cli_command_does_not_exist "sb" "@storybook/cli" ""
+
+  # -----------------------------------------------------------
+else
+  echo "NOTE: snap is not installed! Nothing happened."
+  echo "Read system setup instructions from: https://snapcraft.io/"
+fi
