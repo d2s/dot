92e7ccc6992d8ad22f1c72a8e1d6c67866a77542
Daniel Schildt <daniel@autiomaa.org>
2020-04-05 19:31:01 +0300

    feat(zsh): add more aliases for process management
    - Add more aliases for process management
    
    More details:
    
    - https://github.com/jcs/dotfiles/blob/master/.zshrc

--- zsh/process-management.zsh
+++ zsh/process-management.zsh
@@ -11,6 +11,16 @@ fi
 if [ "$SYSTEMTYPE" = "linux" ] ; then
   # List all processes
   alias processes_all="ps -AFH"
+  alias processes_sorted="ps auwwx | sort -rk 3,3 | head"
+
+  # Usage:
+  #   processes_my_search PATTERN
+  alias processes_my_search="ps auwwx | grep -i -e ^USER -e"
 else
   alias processes_all="ps -Afjv"
+  alias processes_sorted='ps auwwx | head'
+
+  # Usage:
+  #   processes_my_search PATTERN
+  alias processes_my_search="ps auwwx | grep -i -e ^USER -e"
 fi
